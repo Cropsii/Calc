fb476fba2b74967665c5052c30484827ef5f4b86
Cropsi <soap.duskyla.81@mail.ru>
2024-12-07 23:48:50 +0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Button({ num, className ,onClick}) {
+  return (
+    <button
+    onClick={onClick}
+      className={
+        num !== 0
+          ? `bg-[#2a2a2c] rounded-full text-center aspect-square ${className}`
+          : `bg-[#2a2a2c] rounded-full text-center  ${className}`
+      }
+    >
+      {num}
+    </button>
+  );
+}
